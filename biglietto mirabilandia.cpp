f21336b8246c8545ad0094460434c1fc5e768176
Monsterswag <monstersuegprince@hotmail.com>
2016-11-21 12:54:48 +0100

    Add files via upload

---
+++ biglietto mirabilandia.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+	float prezzo,accomp;
+	int a,altezza,eta,sup,accomp
+	printf("premere 1 se e' disabile premere 0 se non e' disabile");
+	scanf("%d",&a); // variabile che indica se e' disabile
+	if(a==1) {
+		prezzo=0,00
+	}
+}

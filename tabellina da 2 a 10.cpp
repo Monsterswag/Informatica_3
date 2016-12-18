6f664c904813b34b27761ace2c4f68c1e8bc273d
Monsterswag <monstersuegprince@hotmail.com>
2016-12-18 23:54:21 +0100

    Add files via upload

---
+++ tabellina da 2 a 10.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+int n,I,tab;
+n=2;
+    while(n<=10){
+    I=1;
+    tab=0;
+        while(I<=10){
+        tab=tab+n;
+        printf("%d",tab);
+        I++;
+        }
+    printf("\n");
+    n++;
+    }  
+}
+

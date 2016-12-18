9126135dd63ecd5b052cb66b6062905774899417
Monsterswag <monstersuegprince@hotmail.com>
2016-12-18 23:57:07 +0100

    Update tabellina da 2 a 10.cpp

--- tabellina da 2 a 10.cpp
+++ tabellina da 2 a 10.cpp
@@ -4,12 +4,12 @@ int main (){
 int n,I,tab;
 n=2;
     while(n<=10){
-    I=1;
-    tab=0;
-        while(I<=10){
-        tab=tab+n;
-        printf("%d",tab);
-        I++;
+         I=1;
+         tab=0;
+         while(I<=10){
+              tab=tab+n;
+              printf("%d",tab);
+              I++;
         }
     printf("\n");
     n++;
